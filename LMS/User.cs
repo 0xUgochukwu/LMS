e6c25425bc603a5592a92e3fde07983aa1e6a940
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-01-16 13:37:23 +0100

    Add User Class

---
+++ LMS/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LMS
+{
+    abstract class User
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string userID;
+        private string password;
+
+        public User(string firstName, string lastName, int age, string password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.password = password;
+            Random getID = new Random();
+            this.userID = Convert.ToString(getID.Next(10000, 99999));
+        }
+
+        public void login()
+        {
+
+        }
+
+        public void logout()
+        {
+
+        }
+
+        public void listBooksInventory()
+        {
+
+        }
+    }
+}
+
